7df1c105f51aed12c696141eb0b624410c19b6da
Magnus <mp-se@noreply.github.com>
2025-06-08 09:39:31 +0200

    Fix issue in mock server

--- mock-server/index.js
+++ mock-server/index.js
@@ -174,8 +174,8 @@ app.get('/api/format', (req, res) => {
   res.send(formatData)
 })
 
-app.post('/api/format2', (req, res) => {
-  console.log('POST: /api/format2')
+app.post('/api/format', (req, res) => {
+  console.log('POST: /api/format')
   /* 
    * Description:    Update the format data that is in body
    * Authentication: Required
