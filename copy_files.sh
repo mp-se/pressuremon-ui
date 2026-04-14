c0d287a676af675363e06c97a6cd85e9dd3f6fde
Magnus <51722613+mp-se@users.noreply.github.com>
2026-04-14 08:39:19 +0200

    feat: sync component and test files from gravitymon-ui
    - Update copy_files.sh to include test file copies
    - Sync fragment components and tests (ListFiles, AdvancedFiles, Voltage, EnableCors)
    - Sync view components and tests (Firmware, Serial, NotFound, DeviceWifi, DeviceWifi2, Tools)
    - All 555 tests passing with 109 skipped

--- copy_files.sh
+++ copy_files.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
+#
+# Pressuremon - Dual License
+#
+# Open Source Use  ->  GNU General Public License v3.0 (GPL v3)
+# Commercial Use   ->  Commercial License Agreement
+#
+# Copyright (c) 2024-2026 Magnus
+#
+# For full license terms and conditions, see LICENSE and LICENSE_COMMERCIAL files.
+# Source: https://github.com/mp-se/pressuremon and https://github.com/mp-se/pressuremon-ui
+#
 
-# Used to copy the views that are standard across all my projects
+# Used to copy the views and tests that are standard across all my projects
 
+# Fragment Components
 cp ../gravitymon-ui/src/fragments/AdvancedFilesFragment.vue ./src/fragments/AdvancedFilesFragment.vue
 cp ../gravitymon-ui/src/fragments/ListFilesFragment.vue ./src/fragments/ListFilesFragment.vue
 cp ../gravitymon-ui/src/fragments/VoltageFragment.vue ./src/fragments/VoltageFragment.vue
 cp ../gravitymon-ui/src/fragments/EnableCorsFragment.vue ./src/fragments/EnableCorsFragment.vue
 
+# Fragment Tests
+cp ../gravitymon-ui/src/fragments/__tests__/AdvancedFilesFragment.test.js ./src/fragments/__tests__/AdvancedFilesFragment.test.js
+cp ../gravitymon-ui/src/fragments/__tests__/ListFilesFragment.test.js ./src/fragments/__tests__/ListFilesFragment.test.js
+cp ../gravitymon-ui/src/fragments/__tests__/VoltageFragment.test.js ./src/fragments/__tests__/VoltageFragment.test.js
+cp ../gravitymon-ui/src/fragments/__tests__/EnableCorsFragment.test.js ./src/fragments/__tests__/EnableCorsFragment.test.js
+
+# View Components
 cp ../gravitymon-ui/src/views/FirmwareView.vue ./src/views/FirmwareView.vue
 cp ../gravitymon-ui/src/views/SerialView.vue ./src/views/SerialView.vue
 cp ../gravitymon-ui/src/views/NotFoundView.vue ./src/views/NotFoundView.vue
 cp ../gravitymon-ui/src/views/DeviceWifiView.vue ./src/views/DeviceWifiView.vue
 cp ../gravitymon-ui/src/views/DeviceWifi2View.vue ./src/views/DeviceWifi2View.vue
 cp ../gravitymon-ui/src/views/ToolsView.vue ./src/views/ToolsView.vue
+
+# View Tests
+cp ../gravitymon-ui/src/views/__tests__/FirmwareView.test.js ./src/views/__tests__/FirmwareView.test.js
+cp ../gravitymon-ui/src/views/__tests__/SerialView.test.js ./src/views/__tests__/SerialView.test.js
+cp ../gravitymon-ui/src/views/__tests__/NotFoundView.test.js ./src/views/__tests__/NotFoundView.test.js
+cp ../gravitymon-ui/src/views/__tests__/DeviceWifiView.test.js ./src/views/__tests__/DeviceWifiView.test.js
+cp ../gravitymon-ui/src/views/__tests__/DeviceWifi2View.test.js ./src/views/__tests__/DeviceWifi2View.test.js
+cp ../gravitymon-ui/src/views/__tests__/ToolsView.test.js ./src/views/__tests__/ToolsView.test.js
+
+echo "✓ Components and tests synced from gravitymon-ui"
--- src/fragments/ListFilesFragment.vue
+++ src/fragments/ListFilesFragment.vue
@@ -1,13 +1,22 @@
 <!--
-  Pressuremon - Dual License
+  GravityMon
+  Copyright (c) 2021-2026 Magnus
 
-  Open Source Use  ->  GNU General Public License v3.0 (GPL v3)
-  Commercial Use   ->  Commercial License Agreement
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-  Copyright (c) 2024-2026 Magnus
+  Alternatively, this software may be used under the terms of a
+  commercial license. See LICENSE_COMMERCIAL for details.
 
-  For full license terms and conditions, see LICENSE and LICENSE_COMMERCIAL files.
-  Source: https://github.com/mp-se/pressuremon and https://github.com/mp-se/pressuremon-ui
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <template>
   <h5>{{ title }}</h5>
